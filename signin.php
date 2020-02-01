db261d1db049d7b92a1aee1c06af47e0acab3031
Prachiagarwal5678 <53116545+Prachiagarwal5678@users.noreply.github.com>
2020-02-01 18:23:00 +0530

    Add files via upload

--- signin.php
+++ signin.php
@@ -5,33 +5,46 @@
 	<link rel="stylesheet" type="text/css" href="css/signin.css">
 </head>
 <body>
-	<div class="signin-form">
+	<br>
+		<div class="signin-form">
 		<form action="" method="POST">
-			<h1>Sign In</h1>
-			<h3>Login into ChatApp</h3>
+		<legend><h1><u>Sign In</u></h1></legend>
+
+	
+
+			
 			</div>
 			<div class="form_group">
-				<label>Email</label><br>
+				<br>
+				
 				<input type="email" name="email" class="form_control" placeholder="example@site.com" required autocomplete="off">
 			</div>
 			
 			<div class="form_group">
-				<label>Password</label><br>
+				<br>
+				
 				<input type="password" name="pass" class="form_control" placeholder="Enter password" required autocomplete="off">
 			</div>
 			<div class="forgot">
-				Forgot password<br>
+				<br>
+				Forgot password:
+				&nbsp
+
 				<a href="forgot_pass.php">Click Here</a>
 			</div>
+			<br>
 			<div class="form_group">
 				<input type="submit" name="sign_in" class="form_control" placeholder="Login">
 			</div>
 			<?php ?>
 		</form>
+		
 		<div class="no_acc">
-			Don't have an account 
+			<br>
+			Don't have an account:
 			<a href="signup.php">create one!</a>
 		</div>
+		
 </body>
 
 </html>
